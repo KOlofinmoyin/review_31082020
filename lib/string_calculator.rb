05ac3cc0be5168902267a04df6dd5ade17cabba6
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-31 15:10:04 +0100

    Sixth test checking additions '+' is 'green'.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,15 @@
 
 def string_calculator(figure)
   # figure == "" ? ["", 0] : figure == "1" ? ["1", 1] : ["2", 2]
-  input = figure
   result = []
+  result.push(figure)
+  figure.split(" ")
+    sum = figure.split(" ")
+if sum.include?("+")
+  input = sum[0].to_i + sum[2].to_i
+else
+  input = figure.to_i
+end
+
   result.push(input)
-    result.push(input.to_i)
 end
