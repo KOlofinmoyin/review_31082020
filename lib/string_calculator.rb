4f626a52ff66bb911495649b1b8423e75c45d4b7
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-31 13:43:40 +0100

    Fifth test green.'

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,4 @@
 
 def string_calculator(figure)
-  figure == "" ? ["", 0] : ["1", 1]
+  figure == "" ? ["", 0] : figure == "1" ? ["1", 1] : ["2", 2] 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,15 +2,15 @@ require 'string_calculator.rb'
 
 describe "#string_calculator" do
 
-  xit 'takes "1" and returns ["1"]' do
-    expect(string_calculator("1")).to eq(["1"])
-  end
-
   it 'takes "" and returns ["",0]' do
     expect(string_calculator("")).to eq(["", 0])
   end
 
   it 'takes "1" and returns ["1", 1]' do
     expect(string_calculator("1")).to eq(["1", 1])
+  end
+
+  it 'takes "2" and returns ["2", 2]' do
+    expect(string_calculator("2")).to eq(["2", 2])
   end
 end
