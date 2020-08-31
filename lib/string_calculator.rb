8451eebc4dfb24a8f60a27239bed66661b663ba5
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-31 16:22:23 +0100

    Refactored.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,13 +7,13 @@ def string_calculator(figure)
     sum = figure.split(" ")
 
 if sum.include?("+")
-  input = sum[0].to_i + sum[2].to_i
+  sum[0].valid_float? || sum[2].valid_float? ? input = sum[0].to_f + sum[2].to_f : input = sum[0].to_i + sum[2].to_i
 elsif sum.include?("-")
   sum[0].valid_float? || sum[2].valid_float? ? input = sum[0].to_f - sum[2].to_f : input = sum[0].to_i - sum[2].to_i
 elsif sum.include?("/")
-  input = sum[0].to_i / sum[2].to_i
+  sum[0].valid_float? || sum[2].valid_float? ? input = sum[0].to_f / sum[2].to_f : input = sum[0].to_i / sum[2].to_i
 elsif sum.include?("*")
-  input = sum[0].to_i * sum[2].to_i
+  sum[0].valid_float? || sum[2].valid_float? ? input = sum[0].to_f * sum[2].to_f : input = sum[0].to_i * sum[2].to_i
 else
   input = figure.to_i
 end
@@ -23,5 +23,6 @@ def valid_float?
   # okay with "truthy" values (like 0.0), you can remove it.
   !!Float(self) rescue false
 end
+
   result.push(input)
 end
