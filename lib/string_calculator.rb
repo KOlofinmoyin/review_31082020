a73163815d0aabae3c2b896969ddb4f0ef427d95
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-31 13:53:03 +0100

    Refactor.'

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,8 @@
 
 def string_calculator(figure)
-  figure == "" ? ["", 0] : figure == "1" ? ["1", 1] : ["2", 2] 
+  # figure == "" ? ["", 0] : figure == "1" ? ["1", 1] : ["2", 2]
+  input = figure
+  result = []
+  result.push(input)
+    result.push(input.to_i)
 end
